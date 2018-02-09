8566c13f0aff1e919904763c4335029cc869cf9e
Rijnard van Tonder <rvantonder@gmail.com>
2018-02-09 10:17:22 -0500

    with printer

--- hw-4/SignAnalysis.java
+++ hw-4/SignAnalysis.java
@@ -17,6 +17,9 @@ import soot.Value;
 import soot.*;
 import soot.toolkits.scalar.*;
 import soot.toolkits.graph.*;
+import soot.NormalUnitPrinter;
+import java.util.*;
+import soot.jimple.internal.*;
 
     /** This analysis identifies the sign of integer variables
      * and warns when a definitely or possibly negative variable
@@ -27,8 +30,24 @@ public class SignAnalysis extends BodyTransformer {
     protected void internalTransform(Body body,
                                      String phaseName,
                                      Map options) {
-        new GuaranteedDefs(new ExceptionalUnitGraph(body));
-        // print guaranteed defs for each unit
+        NormalUnitPrinter printer = new NormalUnitPrinter(body);
+        GuaranteedDefs g = new GuaranteedDefs(new ExceptionalUnitGraph(body));
+        
+        // print out guaranteed defs for each unit
+       for (Unit unit : body.getUnits()) {
+          Stmt stmt = (Stmt) unit;
+          List<Value> l = g.getGuaranteedDefs(unit);
+          for (Value f : l) {
+              if (f instanceof JimpleLocal) {
+                  System.out.print(f.toString());
+                  System.out.print(",");
+              }
+          }
+          System.out.print("\t" + stmt.getClass() + ": ");
+          stmt.toString(printer);
+          System.out.println(printer.output());
+          printer.output().setLength(0);
+      } 
     }
 
     private static SignAnalysis theInstance = new SignAnalysis();
